12de85f11f7f90eb2e27493b329548761b115408
Bilal <p176095@nu.edu.pk>
2020-12-12 14:23:59 +0500

    forms updated using WTF

--- app.py
+++ app.py
@@ -3,12 +3,26 @@
 
 #imp note, if changes in css file are not updated in browser. Do full relod in browser (relod + holding shift)
 
-
+from flask import request
 from flask import Flask, render_template
+
+from forms import LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
 
 
 username = "Bilal"
+
+@app.route('/login', methods = ["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.is_submitted():
+        print(form.email.data)
+        print(form.password.data)
+    else: 
+        print("not yet")
+    return render_template("login.html", form = form)
+
 @app.route('/')
 #Known as view, because displays what needs to be displayed.
 def home():
---
+++ forms.py
@@ -0,0 +1,27 @@
+#had to seperate our application module "app.py" from forms module.
+
+from flask_wtf import FlaskForm
+
+#importing the required classes needed for fields.
+from wtforms import PasswordField, StringField, SubmitField
+#importing the required validators.
+from wtforms.validators import InputRequired
+
+#For each form in our app we will create a class.
+#Now for our login form we create the below class that inherits FlaskForm
+""" 
+For each possible field, wtforms has associated classes. For this particular example we will only import the fields we need.
+StringField for an email
+PasswordField for a password
+SubmitField for the submit button
+"""
+
+class LoginForm(FlaskForm):
+
+    email = StringField("Email", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    submit = SubmitField("Login")
+# we will make instances of these classes as member variables of our class, 
+# and we will pass the labels of these fields as input to the constructors
+
+    
